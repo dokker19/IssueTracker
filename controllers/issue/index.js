80c02f99ef2cc8af89210be69d6b2c7a0c0e8d37
Kyoungjin Lim <klim30@jhu.edu>
2022-11-29 15:43:46 -0500

    [ADD] edited handlebars

--- controllers/issue/index.js
+++ controllers/issue/index.js
@@ -5,7 +5,47 @@ const router = express.Router()
 const sequelize = require('../../sequelize')
 const Users = require('../../sequelize/models/Users')
 
-const { Issues } = sequelize.models
+// const { Issues } = sequelize.models
+Issues = [
+    {
+    dataValues: {
+        id: 1,
+        ticketname: 'myticket1',
+        description: 'this is the first ticket',
+        status: 3, 
+        urgency: 2,
+        issueDate: '2022-11-11'
+    },},
+    {
+    dataValues: {
+        id: 2,
+        ticketname: 'myticket2',
+        description: 'this is the second ticket',
+        status: 1, 
+        urgency: 2,
+        issueDate: '2022-11-12'
+    },},
+    {
+    dataValues: 
+    {
+        id: 3,
+        ticketname: 'myticket3',
+        description: 'this is the third ticket',
+        status: 3, 
+        urgency: 5,
+        issueDate: '2022-11-13'
+    },}, 
+    {
+    dataValues: 
+    {
+        id: 4,
+        ticketname: 'myticket4',
+        description: 'this is the third ticket',
+        status: 3, 
+        urgency: 5,
+        issueDate: '2022-11-13'
+    },}, 
+]
 
 
 
@@ -41,12 +81,19 @@ router.get('/showAll', (req, res) => {
 
 //Index
 router.get('/', (req, res, next) => {
-    Issues.findAll()
-        .then(issues => {
-            res.render('showIssues', {
-                issues
-            })
-        }).catch(err=>console.log(err))
+    res.render('showIssues', {
+        issues: Issues,
+        style: 'custom.css', 
+    })
+
+
+
+    // Issues.findAll()
+    //     .then(issues => {
+    //         res.render('showIssues', {
+    //             issues
+    //         })
+    //     }).catch(err=>console.log(err))
 })
 
 //New
