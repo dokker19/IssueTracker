d1427fcb6839eba9b532bfb5ea44a2fdce97f86a
dokyungyang <dkyang1023@gmail.com>
2023-01-21 23:02:38 -0500

    make it better

--- sequelize/index.js
+++ sequelize/index.js
@@ -18,6 +18,12 @@ const sequelize = new Sequelize('df9j615lkn0oqc', 'cjsrxlmiusjgel', '852a9833cda
     host: 'ec2-52-54-212-232.compute-1.amazonaws.com',
     dialect: 'postgres',
 	timezone: "+00:00",
+	dialectOptions:{
+		ssl:{
+			require:true,
+			rejectUnauthorized:false
+		}
+	}
 })
 
 const modelDefiners = [
