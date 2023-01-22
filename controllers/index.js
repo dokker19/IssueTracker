0eb1584238ca92a32a0a68040e3062aa3e25765a
dokyungyang <dkyang1023@gmail.com>
2023-01-21 21:12:00 -0500

    dashboard styling

--- controllers/index.js
+++ controllers/index.js
@@ -4,7 +4,10 @@ const { Router } = require('express')
 const router = Router()
 
 router.get('/', (req, res) => {
-    res.send('Welcome to the Issue Tracker')
+    res.send(`Welcome to the Issue Tracker\n\n
+    <a href="/login" class="nav-link px-0 align-middle text-white">Get started</a>
+    <br/>
+    <a href="https://github.com/dokker19/IssueTracker">View github</a>`)
 })
 
 router.use('/dashboard', require('./dashboard'))
